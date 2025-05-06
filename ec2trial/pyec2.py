537ab55d4cc4980884270e034d828a227c9530b6
BlankParry <kmamogh.ssg@gmail.com>
2025-05-06 12:30:13 +0530

    Add files via upload

---
+++ ec2trial/pyec2.py
@@ -0,0 +1,16 @@
+# filepath: c:\Users\kmamo\Downloads\ec2trial\pyec2.py
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def homedit():
+    return render_template('homedit.html')
+
+@app.route('/newpage', methods=['POST'])
+def newpage():
+    name = request.form['name']
+    return render_template('newpage.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
